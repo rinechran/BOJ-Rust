27a92b8ec9e52023f7eb5a1d8a373c99aaba6c36
eunsik <rinechran@gmail.com>
2024-09-17 19:32:43 +0900

    feat : 10869.rs

---
+++ 10000/10869.rs
@@ -0,0 +1,17 @@
+use std::io;
+fn main(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let numbers : Vec<i32> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!("{}",numbers[0] + numbers[1]);
+    println!("{}",numbers[0] - numbers[1]);
+    println!("{}",numbers[0] * numbers[1]);
+    println!("{}",numbers[0] / numbers[1]);
+    println!("{}",numbers[0] % numbers[1]);
+
+}
