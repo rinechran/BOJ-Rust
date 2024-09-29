f87408cfdfba62c460e41b600faca640a5214feb
eunsik <rinechran@gmail.com>
2024-09-30 01:35:39 +0900

    feat : 24723.rs

---
+++ 24000/24723.rs
@@ -0,0 +1,18 @@
+use std::io::stdin;
+
+fn input_user_to_vec<T: std::str::FromStr>() -> Vec<T> {
+    let mut input_string = String::new();
+    stdin().read_line(&mut input_string).unwrap();
+
+    input_string
+        .split_ascii_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+fn main() {
+    let first_input: Vec<u32> = input_user_to_vec();
+    let n = first_input[0];
+
+    println!("{}",2_i32.pow(n));
+}
