ce356115a435e2698ffdabc1e0b9273ca117ac6b
eunsik <rinechran@gmail.com>
2024-12-08 23:51:40 +0900

    feat : 10817.rs

---
+++ 10000/10817.rs
@@ -0,0 +1,19 @@
+use std::io::{self, stdin};
+
+fn input_user_to_vec<T: std::str::FromStr>() -> Vec<T> {
+    let mut input_string = String::new();
+    stdin().read_line(&mut input_string).unwrap();
+
+    input_string
+        .split_ascii_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+fn main() {
+    let mut input: Vec<i32> = input_user_to_vec();
+    input.sort();
+
+    println!("{}",input[1]);
+
+}
