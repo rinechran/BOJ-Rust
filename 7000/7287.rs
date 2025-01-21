53aff87fa504375dec023686fb07ad6984f5f276
eunsik <rinechran@gmail.com>
2025-01-21 23:32:27 +0900

    feat : 7287.rs

---
+++ 7000/7287.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn input_user_to_vec<T: std::str::FromStr>() -> Vec<T> {
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).unwrap();
+    input_string
+        .trim()
+        .split_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+
+fn main() {
+
+    println!("{} ",300);
+    println!("{} ","rinechran");
+
+}
